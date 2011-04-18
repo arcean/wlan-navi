2fd505841ae5f0cae903270c0320082e4710990e
Bartosz Kolada <Kaef@localhost.localdomain>
2011-04-18 22:02:11 +0200

    nadpisałem to co bylo nie działające okienka

--- main.cpp
+++ main.cpp
@@ -1,14 +1,29 @@
-#include <QtGui/QApplication>
 #include "mainwindow.h"
+
+#include <QApplication>
+#include <QList>
+#include <QString>
+#include <QUrl>
+#include <QSettings>
+#include <QProcessEnvironment>
+#include <QNetworkProxyFactory>
+
+#include "qgeoserviceprovider.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-#if defined(Q_WS_S60)
-    w.showMaximized();
+
+    QApplication::setOrganizationName("K0l40a");
+    QApplication::setApplicationName("wlan-navi");
+
+    MainWindow mw;
+
+    mw.resize(200,200);
+#if defined(Q_OS_SYMBIAN) || defined(Q_WS_MAEMO_5) || defined(Q_WS_SIMULATOR)
+    mw.showMaximized();
 #else
-    w.show();
+    mw.show();
 #endif
 
     return a.exec();
